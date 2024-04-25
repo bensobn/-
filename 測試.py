b908624b6c46634fe66e8ac6d5cd5775f9fdf927
bensobn <bbwan110@gmail.com>
2024-04-25 23:57:02 +0800

    Delete 測試.py

--- 測試.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-a = np.zeros((15,15))
-for i in range(-7,8):
-    for k in range(-7,8):
-        a[i+7][k+7] = abs(i)+13*abs(k)
-np.save("C:/Users/morga/Desktop/程式/音樂資料/計算.npy", a)
